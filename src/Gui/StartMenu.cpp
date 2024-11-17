f38689cc730cfe6f580d8143ee25af763abd29dd
Ricardo <ricardo.locher123@gmail.com>
2024-11-17 11:26:07 +0100

    Bugfixes. Window was not reacting to any interaction except arrow keys during start menu

--- src/Gui/StartMenu.cpp
+++ src/Gui/StartMenu.cpp
@@ -41,33 +41,42 @@ StartMenu::StartMenu(sf::RenderWindow& window) : m_window(window), m_selectedInd
 
 int StartMenu::displayMenu() {
     backgroundMusic.play();
+
     while (m_window.isOpen()) {
-        handleInput();
+        sf::Event event;
+        while (m_window.pollEvent(event)) {
+            // Handle window close event
+            if (event.type == sf::Event::Closed) {
+                m_window.close();
+                return -1; // Exit the menu if the window is closed
+            }
+
+            // Handle key press events
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Up) {
+                    m_selectedIndex = (m_selectedIndex - 1 + m_menuItems.size()) % m_menuItems.size();
+                } else if (event.key.code == sf::Keyboard::Down) {
+                    m_selectedIndex = (m_selectedIndex + 1) % m_menuItems.size();
+                } else if (event.key.code == sf::Keyboard::Enter) {
+                    return m_selectedIndex;
+                }
+            }
+        }
+
+        // Update menu item colors
+        for (size_t i = 0; i < m_menuItems.size(); ++i) {
+            m_menuItems[i].setFillColor(i == m_selectedIndex ? sf::Color::Yellow : sf::Color::White);
+        }
+
+        // Render the menu
         m_window.clear();
         background.draw(m_window);
         m_window.draw(titleSprite);
         renderMenu();
         m_window.display();
-
-        // Return the selected option if the user presses Enter
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) {
-            return m_selectedIndex;
-        }
     }
+
     return -1; // Return -1 if the window is closed
-}
-
-void StartMenu::handleInput() {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-        m_selectedIndex = (m_selectedIndex - 1 + m_menuItems.size()) % m_menuItems.size();
-    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-        m_selectedIndex = (m_selectedIndex + 1) % m_menuItems.size();
-    }
-
-    // Update colors
-    for (size_t i = 0; i < m_menuItems.size(); ++i) {
-        m_menuItems[i].setFillColor(i == m_selectedIndex ? sf::Color::Yellow : sf::Color::White);
-    }
 }
 
 void StartMenu::renderMenu() {
